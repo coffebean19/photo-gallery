5e6c39f73464b333e3512576619331fbd903d9fa
Coffebean <istvan.mayer1999@protonmail.com>
2021-11-22 02:05:09 +0200

    Implemented login capabilities

--- app.js
+++ app.js
@@ -38,10 +38,79 @@ app.get('/login', (req, res) => {
 app.get('/login/:username&:password', (req, res) => {
     try
     {
-        console.log(req.params);
-        res.status(200).json({
-            logged: "false"
-        });
+        let username = req.params.username;
+        let password = req.params.password;
+        let dbPassword = "";
+
+        var Connection = require('tedious').Connection;  
+        var config = {  
+            server: 'project-323.database.windows.net',  //update me
+            authentication: {
+                type: 'default',
+                options: {
+                    userName: 'coffeebean', //update me
+                    password: 'Simpel projek!2'  //update me
+                }
+            },
+            options: {
+                // If you are on Microsoft Azure, you need encryption:
+                encrypt: true,
+                database: 'photo-gallery'  //update me
+            }
+        }; 
+        var connection = new Connection(config);  
+        connection.on('connect', function(err) {  
+            // If no error, then good to proceed.  
+            console.log("Connected");  
+            executeStatement();  
+        });  
+        
+        connection.connect();
+      
+        var Request = require('tedious').Request;  
+        var TYPES = require('tedious').TYPES;  
+      
+        function executeStatement() {  
+            request = new Request(`SELECT * FROM dbo.[User] WHERE [Username]='${username}'`, function(err) {  
+            if (err) {  
+                console.log(err);}  
+            });  
+            var result = "";  
+            request.on('row', function(columns) {  
+                columns.forEach(function(column) {  
+                  if (column.value === null) {  
+                    console.log('NULL');  
+                  } else {  
+                    result+= column.value + " ";  
+                  }  
+                });  
+                console.log(result);
+                dbPassword = columns[2].value.trim();
+                result ="";  
+            });  
+      
+            request.on('done', function(rowCount, more) {  
+            console.log(rowCount + ' rows returned');  
+            });  
+            
+            // Close the connection after the final event emitted by the request, after the callback passes
+            request.on("requestCompleted", function (rowCount, more) {
+                connection.close();
+
+                console.log(`Is ${dbPassword} == ${password}? ${ dbPassword == password }`)
+                if (dbPassword == password) {
+                    res.status(200).json({
+                        logged: "true"
+                    })
+                }else {
+                    res.status(200).json({
+                        logged: "false"
+                    });
+                }
+            });
+            connection.execSql(request);  
+        }          
+
     } catch(err) {
         res.status(400).json({
             message: "Some error occured",
@@ -131,9 +200,6 @@ app.get('/signup/:username&:password', (req, res) => {
         })
     }
 })
-
-
-
 
 app.listen(port, () => {
     console.log('Listening over port:  ' + port);
